95b67f987778b06095cac2a0003583fd59168df8
airetOK <sergey.bezrukavyy@gmail.com>
2022-03-10 00:00:44 +0200

    update all server.js

--- Portfolio/server.js
+++ Portfolio/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static('/dist/portfolio'));
+app.use(express.static('Portfolio/dist/portfolio'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join('/dist/portfolio/index.html'));});
+res.sendFile(path.join('Portfolio/dist/portfolio/index.html'));});
 app.listen(process.env.PORT || 4200);
